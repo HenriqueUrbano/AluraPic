d97f1f9803b1184763c2d94090f672a395561e2a
Henrique Urbano <henriqueurbano21@gmail.com>
2019-02-03 22:29:59 -0200

    Commit Inicial

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,16 @@
+angular.module('alurapic').controller('FotosController', function($http, $scope) {
+	
+	$scope.foto = [{
+		titulo : 'Leão',
+		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+	},
+	{
+		titulo : 'Leão2',
+		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+	},
+	{
+		titulo : 'Leão3',
+		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
+	}]; 
+
+});
