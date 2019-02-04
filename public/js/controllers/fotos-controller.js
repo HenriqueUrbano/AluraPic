a4bc3d2a7e78c910ee18c0dda09a84cdcc9990bd
Henrique Urbano <henriqueurbano21@gmail.com>
2019-02-03 22:48:15 -0200

    Inclusão de comentários e requisição assíncrona no back-end

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,16 +1,29 @@
-angular.module('alurapic').controller('FotosController', function($http, $scope) {
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
 	
-	$scope.foto = [{
-		titulo : 'Leão',
-		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	},
-	{
-		titulo : 'Leão2',
-		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	},
-	{
-		titulo : 'Leão3',
-		url : 'http://www.fundosanimais.com/Minis/leoes.jpg'
-	}]; 
+	//Declaração inicial da variávei Scope
+	$scope.foto = []; 
+
+	//Realiza uma requisição assíncrona para obter os dados
+	$http.get('v1/fotos')
+	//Executa caso a requisição ocorra com suceso
+	.success(function(fotos){
+		$scope.foto = fotos;
+	})
+	//Executa caso a requisição NÃO ocorra com sucesso
+	.error(function(error){
+		console.log(error);
+	});
+
+	/* VERSÃO COM PROMISE
+	//Realiza uma requisição assíncrona para obter os dados
+	var promise = $http.get('v1/fotos');
+	//Executa caso a requisição ocorra com suceso
+	promise.then(function(retorno){
+		$scope.foto = retorno.data;
+	}).catch(function(erro){
+		//Função chamada caso a requisição não ocorra com sucesso
+		console.log(error);
+	});
+	*/
 
 });
