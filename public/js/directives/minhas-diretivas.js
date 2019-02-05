94094160d98f5c115908209233fddd1168b5293d
Henrique Urbano <henriqueurbano21@gmail.com>
2019-02-04 22:40:37 -0200

    Criação de diretivas customizadas

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,20 @@
+angular.module('minhasDiretivas', [])
+.directive('meuPainel',function(){
+
+    var ddo = {};
+
+    //Restrições da Diretiva (A = Atribute, E = Element)
+    ddo.restrict ="AE";
+
+    //Escopo privado da diretiva (O que a diretiva possui dentro de si)
+    ddo.scope ={
+        titulo:'@'
+    };
+    //Permite a inclusão de elementos filhos em nossa diretiva customizada
+    ddo.transclude = true;
+
+    //Template da diretiva (Diretiva ng-transclude necessária para inclusão de elemento filho na div)
+    ddo.templateUrl='js/directives/meu-painel.html';
+    //Toda diretiva precisa retornar uma DDO (Directive Definition Object)
+    return ddo;
+});
