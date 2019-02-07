c2d1ef3286943c4eb35384e5d4f5a05000e01827
Henrique Urbano <henriqueurbano21@gmail.com>
2019-02-06 22:08:50 -0200

    Inclusão de validação e mensagens de erro no formulário

--- public/js/controllers/cadastro-controller.js
+++ public/js/controllers/cadastro-controller.js
@@ -1,9 +1,20 @@
-angular.module('alurapic').controller('CadastroController', function($scope){
+angular.module('alurapic').controller('CadastroController', function($scope, $http){
     
     $scope.foto = {};
-
+    $scope.mensagem = '';
+    
     $scope.submeter = function(){
-        console.log($scope.foto);
-    }
+        if($scope.formulario.$valid){
+            $http.post('v1/fotos', $scope.foto)
+            .success(function(){
+                $scope.foto = {};
+                $scope.mensagem= 'Foto cadastrada com sucesso!';
+            })
+            .error(function(erro){
+                $scope.mensagem= 'Não foi possível incluir a foto!';
+                console.log(erro);
+            });
+        };
+    };
 
 });
