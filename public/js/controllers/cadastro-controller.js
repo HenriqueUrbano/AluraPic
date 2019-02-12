569b737cee4c862d621882e6de527372dfbd9cfa
Henrique <henriqueurbano21@gmail.com>
2019-02-12 12:01:34 -0200

    Criação de um serviço Angular para criação e edição das fotos

--- public/js/controllers/cadastro-controller.js
+++ public/js/controllers/cadastro-controller.js
@@ -1,4 +1,4 @@
-angular.module('alurapic').controller('CadastroController', function($scope, $http, $routeParams, recursoFoto){
+angular.module('alurapic').controller('CadastroController', function($scope, $routeParams, cadastroDeFotos){
     
     $scope.foto = {};
     $scope.mensagem = '';
@@ -21,43 +21,16 @@ angular.module('alurapic').controller('CadastroController', function($scope, $ht
 
     $scope.submeter = function(){
         if($scope.formulario.$valid){
-            if($scope.foto._id){
-
-                recursoFoto.update({fotoId : $scope.foto._id}, $scope.foto, function(){
-                    $scope.mensagem= 'Foto ' + $scope.foto.titulo + ' alterada com sucesso!';
-                }, function(erro){
-                    $scope.mensagem= 'Não foi possível alterar a foto' + $scope.foto.titulo + '!';
-                    console.log(erro);
-                });
-
-                /* $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
-                .success(function(){
-                    
-                })
-                .error(function(erro){
-                    
-                }); */
-            } else {
-                
-                recursoFoto.save($scope.foto, function(){
+            cadastroDeFotos.cadastrar($scope.foto)
+            .then(function(dados){
+                $scope.mensagem = dados.mensagem;
+                if(dados.inclusao){
                     $scope.foto = {};
-                    $scope.mensagem = 'Foto Incluída com Sucesso!';
-                }, function(erro){
-                    console.log(erro);
-                    $scope.mensagem='Não foi possível incluir a foto!';
-                });
-
-                /* $http.post('v1/fotos', $scope.foto)
-                .success(function(){
-                    $scope.foto = {};
-                    $scope.mensagem= 'Foto cadastrada com sucesso!';
-                })
-                .error(function(erro){
-                    $scope.mensagem= 'Não foi possível incluir a foto!';
-                    console.log(erro);
-                }); */
-            };
+                }
+            })
+            .catch(function(dados){
+                $scope.mensagem = dados.mensagem;
+            });
         };
     };
-
 });
